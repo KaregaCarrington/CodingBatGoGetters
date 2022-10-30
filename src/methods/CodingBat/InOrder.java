e3777d5c0bc98dc91e3f68c15cfb81792dae7de3
User <user@Users-MBP.lan>
2022-10-30 19:19:17 -0400

    completed coding bat inOrder challenge

---
+++ src/methods/CodingBat/InOrder.java
@@ -0,0 +1,25 @@
+package methods.CodingBat;
+
+public class InOrder {
+    public static void main(String[] args) {
+
+    /*
+
+    Given three ints, a b c, return true if b is greater than a,
+    and c is greater than b. However, with the exception that if "bOk"
+    is true, b does not need to be greater than a.
+
+    inOrder(1, 2, 4, false) → true
+    inOrder(1, 2, 1, false) → false
+    inOrder(1, 1, 2, true) → true
+     */
+
+    }
+
+    public  static boolean inOrder(int a, int b, int c, boolean bOk) {
+        if(bOk){
+            return c > b;
+        }
+        return a < b && b < c;
+    }
+}
